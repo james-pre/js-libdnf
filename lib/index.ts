299034881d0f5744722f6d97c335f6d7cc92ec2e
James Prevett <jp@jamespre.dev>
2026-06-29 10:54:57 -0500

    Fixed inline type imports

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,12 @@
 import dnf5 from './libdnf5.node';
 import type * as classes from './_classes.js';
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { Group, Package } from './data.js';
+import type { GroupQueryFilter, PackageQueryFilter } from './query.js';
+import type { TransactionInit } from './transaction.js';
+/* eslint-enable @typescript-eslint/no-unused-vars */
+
 export const { Transaction, loadRepos, query, queryGroups, transaction } = dnf5;
 export type Transaction = classes.Transaction;
 
